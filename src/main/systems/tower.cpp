b7e6f0764f250e959a212efdf2a538587b5c2354
Life4gal <life4gal@gmail.com>
2025-09-04 19:12:10 +0800

    移除多余的内容

--- src/main/systems/tower.cpp
+++ src/main/systems/tower.cpp
@@ -20,30 +20,5 @@ namespace systems
 
 			window.draw(shape.shape);
 		}
-
-		if (auto* render = registry.ctx().find<tower::DebugRenderTarget>())
-		{
-			auto& lines = render->lines;
-
-			for (const auto tower_view = registry.view<const tower::Target, tags::tower>();
-			     const auto [entity, target]: tower_view.each())
-			{
-				// 目标可能已经被别的塔击杀
-				// if (registry.all_of<tags::dead>(target.entity))
-				if (not registry.valid(target.entity))
-				{
-					continue;
-				}
-
-				const auto [tower_position] = registry.get<const entity::Position>(entity);
-				const auto [enemy_position] = registry.get<const entity::Position>(target.entity);
-
-				lines.append({.position = tower_position, .color = sf::Color::Red, .texCoords = {}});
-				lines.append({.position = enemy_position, .color = sf::Color::Red, .texCoords = {}});
-			}
-
-			window.draw(lines);
-			lines.clear();
-		}
 	}
 }
