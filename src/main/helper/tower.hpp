a30d8a41540669e3dadf7f97e2d2720ea905baee
Life4gal <life4gal@gmail.com>
2025-09-25 19:41:02 +0800

    chore

--- src/main/helper/tower.hpp
+++ src/main/helper/tower.hpp
@@ -9,6 +9,7 @@ namespace helper
 	class Tower
 	{
 	public:
+		// 构建塔(实体组件),不负责设置塔位置(同时也不设置塔标记)
 		static auto build(entt::registry& registry, components::entity::Type type) noexcept -> entt::entity;
 	};
 }
